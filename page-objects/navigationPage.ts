346d9cedc22cc9c2422d2c3a44f27b39571f4a4d
dtran3612 <169424940+dtran3612@users.noreply.github.com>
2026-03-23 18:51:23 -0700

    page-object-model 3 files added

---
+++ page-objects/navigationPage.ts
@@ -0,0 +1,22 @@
+import {Locator, Page} from "@playwright/test"
+import { HelperBase } from "./helperBase"
+
+export class NavigationPage extends HelperBase{
+    constructor(page: Page){
+        super(page)
+    }
+
+    async formLayoutsPage(){
+        await this.page.getByText('Forms').click()
+        await this.page.getByText('Form Layouts').click()
+        await this.waitForNumberOfSeconds(2)
+    }
+
+    private async selectGroupMenuItem(groupItemTitle: string){
+        const groupMenuItem = this.page.getByTitle(groupItemTitle)
+        const expandedState = await groupMenuItem.getAttribute('aria-expanded')
+        if(expandedState == "false"){
+            await groupMenuItem.click()
+        }
+    }
+}
