945e8fc0ffee89347601844a3bd12b0bd8fefde9
dtran3612 <169424940+dtran3612@users.noreply.github.com>
2026-03-23 19:11:02 -0700

    page-object-model navigationPage working

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -6,17 +6,17 @@ export class NavigationPage extends HelperBase{
         super(page)
     }
 
-    async formLayoutsPage(){
-        await this.page.getByText('Forms').click()
-        await this.page.getByText('Form Layouts').click()
-        await this.waitForNumberOfSeconds(2)
+    async categoriesNewArrivals(){
+        await this.page.getByRole('link', { name: 'Categories' }).click()
+        await this.page.getByRole('link', { name: 'New Arrivals' }).click()
+        await this.waitForNumberOfSeconds(1) // 🚧>1?
     }
 
-    private async selectGroupMenuItem(groupItemTitle: string){
-        const groupMenuItem = this.page.getByTitle(groupItemTitle)
-        const expandedState = await groupMenuItem.getAttribute('aria-expanded')
-        if(expandedState == "false"){
-            await groupMenuItem.click()
-        }
-    }
+    // private async selectGroupMenuItem(groupItemTitle: string){
+    //     const groupMenuItem = this.page.getByTitle(groupItemTitle)
+    //     const expandedState = await groupMenuItem.getAttribute('aria-expanded')
+    //     if(expandedState == "false"){
+    //         await groupMenuItem.click()
+    //     }
+    // }
 }
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,28 +1,27 @@
 import { test, expect } from '@playwright/test';
+import { PageManager } from '../page-objects/pageManager'
 
+// [DONE] ✅
 test.beforeEach(async({page})=>{
   await page.goto('https://target.com/');
 })
 
-test('update shipping location', async ({ page }) => {
-  await page.locator('[data-test="@web/ZipCodeButton/StyledZipCodeButton"]').click()
-  // await page.waitForTimeout(1000) //✅
-
-  const zipInput = page.locator('[data-test="@web/LocationFlyout/FormInput"]')
-  await zipInput.clear()
-  await zipInput.fill('90250')
-
-  // await page.waitForTimeout(1000) //✅
-  await page.locator('[data-test="@web/LocationFlyout/UpdateLocationButton"]').click()
-});
-
-test('navigate categories > new_arrivals', async ({ page }) => {
-  // await page.waitForTimeout(1000) //✅
-  await page.getByRole('link', { name: 'Categories' }).click()
-  // await page.waitForTimeout(1000) //✅
-  await page.getByRole('link', { name: 'New Arrivals' }).click()
-  await page.waitForTimeout(3000)    //🚧>3000
+test('navigate to page', async({page})=> {
+    const pm = new PageManager(page)
+    await pm.navigateTo().categoriesNewArrivals()
 })
+
+// test('update shipping location', async ({ page }) => {
+//   await page.locator('[data-test="@web/ZipCodeButton/StyledZipCodeButton"]').click()
+//   // await page.waitForTimeout(1000) //✅
+
+//   const zipInput = page.locator('[data-test="@web/LocationFlyout/FormInput"]')
+//   await zipInput.clear()
+//   await zipInput.fill('90250')
+
+//   // await page.waitForTimeout(1000) //✅
+//   await page.locator('[data-test="@web/LocationFlyout/UpdateLocationButton"]').click()
+// });
 
 // //[TODO-Later]
 // //Works
